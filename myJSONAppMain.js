2765eb3b2756130623e8154ecfe320c99e7d7f90
naveenshankar <naveenshankarcse@gmail.com>
2014-07-26 22:53:55 -0700

    Update myJSONAppMain.js

--- myJSONAppMain.js
+++ myJSONAppMain.js
@@ -1,17 +1,23 @@
 var myJSONAppMain = function()
 {	
-	
 	var callBack = function(user){
-				
-			 };
+		console.log("inside callback");
+	 };
 
-	$.ajax({
-		  url: "getMyTables",
+	$('.proceed').click(function(){
+		var publicAddress = $('.address').val();
+		console.log(publicAddress);
+
+		$.ajax({
+		  data:publicAddress,
+		  url: "getTransferConfirmation",
 		  type: "GET",
 		  error: function (xhr, ajaxOptions, thrownError) {
-	        callBack(table.get('mockDataTables'));
+	        //callBack(table.get('mockDataTables'));
 	      }
 		}).done(callBack);
+	})
+	
 }
 
 $( document ).ready(function() {
